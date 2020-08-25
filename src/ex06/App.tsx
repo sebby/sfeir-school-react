870ced1fede0b46ec651dd1f13d3163b86d894eb
Sebastien cunin <sebastien.cunin@gmail.com>
2020-08-25 16:40:09 +0200

    Ex06: données chargés et afficher.

--- src/ex06/App.tsx
+++ src/ex06/App.tsx
@@ -1,21 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TopAppBarActionItem } from "@rmwc/top-app-bar";
 
 import { Header } from "../solution/Header";
 import { Loading } from "../solution/Loading";
 import { SearchableList } from "../solution/SearchableList";
+import { loadPeople } from "../utils";
 
 import { Player } from "./Player";
 // import { Player } from "../solution/Player";
 
 export const App: React.FC = () => {
   const [showList, setShowList] = useState(true);
+  const [people, setDatasPeople] = useState([]);
   const toggleView = () => setShowList(x => !x);
   const toggleIcon = showList ? "view_carousel" : "view_module";
 
-  // get people from http://localhost:3000/people
-  // you can use loadPeople in ../utils.js
-  const people = [];
+
+
+  useEffect(() => {
+      loadPeople().then(data => {
+        console.log('Recup. des données json');
+        console.log(data);
+        setDatasPeople(data);
+      })
+    },[]);
+
+  useEffect(() => {
+     console.log('Update sur le array people');
+     console.log(people);
+    },
+    [people]);
+  
 
   const CurrentView: React.ComponentType<{ people: People }> =
     people.length === 0 ? Loading : showList ? SearchableList : Player;
